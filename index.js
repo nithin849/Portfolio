14b68acd71c0f23566a8e6d8872a2af92db3bc14
Mangali Nithin <139614263+nithin849@users.noreply.github.com>
2024-07-25 16:03:43 +0530

    added index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#element', {
+      strings: ['want  to be a web developer', 'want to be a software Engineer'],
+      typeSpeed: 50,
+  });
+
+document.getElementById('githubButton').addEventListener('click', function() {
+  window.location.href = 'https://github.com/nithin849';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var resumeButton = document.getElementById('resumeButton');
+  resumeButton.addEventListener('click', function() {
+      // Create a link element
+      var link = document.createElement('a');
+      link.href = 'Nani.pdf'; 
+      link.download = 'who are you bro.pdf';  //name of file to be downloaded
+      document.body.appendChild(link); // Append the link to the body
+      link.click(); // Trigger the download
+      document.body.removeChild(link); // Remove the link from the body
+  });
+});
